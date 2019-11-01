0a0c9e4084a5b0717268958abb5eb2ec6ba18dce
Sadaqatkhan516 <57184202+Sadaqatkhan516@users.noreply.github.com>
2019-11-01 14:01:23 +0000

    Add files via upload

---
+++ class_example.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php include("class_lib.php"); ?>
+</head>
+<body>
+<?php
+
+	
+	$result1 = new addition("20","15");
+	$stefan = new person("Jason Jones");
+	$jimmy = new person("Billy Jones");
+	
+	echo "first value is : " . $result1->get_first();
+	echo "<br>second value is : " . $result1->get_second();
+	echo "<br>addition of above is : " . $result1->get_result();
+
+	echo "<br>Stefan's full name: " . $stefan->get_name();
+	echo "<br> Nick's full name: " . $jimmy->get_name();
+
+	$jenifer = new employee("Jenny lewis");
+	echo "<br> Jenny is new employee and her name is " . $jenifer->get_name();
+?>
+</body>
+</html>
---
+++ class_lib.php
@@ -0,0 +1,44 @@
+<?php
+class addition
+{
+public $first;
+public $second;
+public $result;
+function __construct($first,$second) {
+	$this->first = $first;
+	$this->second = $second;
+	$this->result = $first + $second;
+	}
+function get_first() {
+	return $this->first;
+	}
+function get_second() {
+	return $this->second;
+	}
+function get_result() {
+	return $this->result;
+	}
+} 
+
+class person 
+{
+var $name;
+function __construct($person_name) {
+	$this->name = $person_name;
+	}
+
+function set_name($new_name) {
+	$this->name = $new_name;
+	}
+
+function get_name(){
+	return $this->name;
+	 }
+}
+
+class employee extends person {
+	function __construct($employee_name) {
+	$this->set_name($employee_name);
+	}
+
+}
